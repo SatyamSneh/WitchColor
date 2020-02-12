4001e9c228d392b7895a25a3b1a25978cfcaf00e
Sneh Satyam <45045634+SatyamSneh@users.noreply.github.com>
2020-02-13 01:26:41 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+# import the modules  
+import tkinter 
+import random 
+  
+# list of possible colour. 
+colours = ['Red','Blue','Green','Pink','Black', 
+           'Yellow','Orange','White','Purple','Brown'] 
+score = 0
+  
+# the game time left, initially 30 seconds. 
+timeleft = 30
